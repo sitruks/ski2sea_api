e6475b0529a734b05edca5c7af1c2e8f9e7d8b2f
SITRUK <kurtis.santillanes@gmail.com>
2020-01-13 15:17:04 -0800

    making further naming changes and updated the html landing page to reflect the Ski2Sea info as opposed to the oauth example

--- wdc/app.js
+++ wdc/app.js
@@ -22,15 +22,6 @@ app.use(cookieParser());                                    // cookieParser midd
 app.use(express.static(__dirname + '/public'));
 
 // -------------------------------------------------- //
-// Variables
-// -------------------------------------------------- //
-var clientID = process.env.FOURSQUARE_CLIENT_ID || config.CLIENT_ID;
-var clientSecret = process.env.FOURSQUARE_CLIENT_SECRET || config.CLIENT_SECRET;
-console.log(clientID);
-console.log(clientSecret);
-var redirectURI = config.HOSTPATH + ":" + config.PORT + config.REDIRECT_PATH
-
-// -------------------------------------------------- //
 // Routes
 // -------------------------------------------------- //
 
@@ -38,55 +29,6 @@ app.get('/', function(req, res) {
   console.log("got here");
   res.redirect('/index.html');
 });
-
-// This route is hit once Foursquare redirects to our
-// server after performing authentication
-app.get('/redirect', function(req, res) {
-  // get our authorization code
-  authCode = req.query.code;
-  console.log("Auth Code is: " + authCode);
-
-  // Set up a request for an long-lived Access Token now that we have a code
-  var requestObject = {
-      'client_id': clientID,
-      'redirect_uri': redirectURI,
-      'client_secret': clientSecret,
-      'code': authCode,
-      'grant_type': 'authorization_code'
-  };
-
-  var token_request_header = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-  };
-
-  // Build the post request for the OAuth endpoint
-  var options = {
-      method: 'POST',
-      url: 'https://foursquare.com/oauth2/access_token',
-      form: requestObject,
-      headers: token_request_header
-  };
-
-  // Make the request
-  request(options, function (error, response, body) {
-    if (!error) {
-      // We should receive  { access_token: ACCESS_TOKEN }
-      // if everything went smoothly, so parse the token from the response
-      body = JSON.parse(body);
-      var accessToken = body.access_token;
-      console.log('accessToken: ' + accessToken);
-
-      // Set the token in cookies so the client can access it
-      res.cookie('accessToken', accessToken, { });
-
-      // Head back to the WDC page
-      res.redirect('/index.html');
-    } else {
-      console.log(error);
-    }
-  });
-});
-
 
 // -------------------------------------------------- //
 // Create and start our server

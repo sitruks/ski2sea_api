9845d2a31b18ce9f37c123eb6b31b2ed0d1769c4
SITRUK <kurtis.santillanes@gmail.com>
2020-01-13 14:24:51 -0800

    removing old file structure

--- wdc/config.js
+++ wdc/config.js
@@ -2,9 +2,9 @@
 // Contains credentials for your Foursquare application
 // And the new redirect path for the OAuth flow
 module.exports = {
- 'HOSTPATH': 'http://localhost',
- 'PORT': 3333,
- 'CLIENT_ID': 'YOUR_CLIENT_ID',
- 'CLIENT_SECRET': 'YOUR_CLIENT_SECRET',
- 'REDIRECT_PATH': '/redirect'
+    'HOSTPATH': 'http://localhost',
+    'PORT': 3333,
+    'CLIENT_ID': 'YOUR_CLIENT_ID',
+    'CLIENT_SECRET': 'YOUR_CLIENT_SECRET',
+    'REDIRECT_PATH': '/redirect'
 };

1bd70a5c50f4889ce96825cb5d4cc9a3431fa817
SITRUK <kurtis.santillanes@gmail.com>
2020-02-05 21:31:20 -0800

    making further clean ups of commented out code

--- wdc/public/ski2sea.js
+++ wdc/public/ski2sea.js
@@ -7,18 +7,6 @@
     // Define the schema
     myConnector.getSchema = function (schemaCallback) {
 
-        // no longer need to define the lengthy object within our javascript itself.
-        // var standardConnections = new Promise(function (resolve, reject) {
-        //     loadJSON("skiToSeaDataJoins", function (json) {
-        //         var obj = JSON.parse(json);
-        //         var connectionList = [];
-        //         for (var connection in obj.connections) {
-        //             connectionList.push(obj.connections[connection]);
-        //         }
-        //         resolve(connectionList);
-        //     }, true);
-        // });
-
         // Schema for results data
         var results_cols = [{
             id: "leg", alias: "Leg", dataType: tableau.dataTypeEnum.string
@@ -39,7 +27,6 @@
         }, {
             id: "ed", alias: "End Time", dataType: tableau.dataTypeEnum.date
         }];
-
         var resultsTable = {
             id: "results", alias: "Results Data", columns: results_cols
         };
@@ -84,7 +71,6 @@
         }, {
             id: "dc", alias: "Division", dataType: tableau.dataTypeEnum.string
         }];
-
         var teamsTable = {
             id: "teams", alias: "Teams Data", columns: teams_cols
         };
@@ -95,7 +81,6 @@
         }, {
             id: "long_name", alias: "Long Name", dataType: tableau.dataTypeEnum.string
         }];
-
         var divisionsTable = {
             id: "divisions", alias: "Divisions Data", columns: divisions_cols
         };
@@ -108,118 +93,12 @@
         }, {
             id: "long_name", alias: "Long Name", dataType: tableau.dataTypeEnum.string
         }];
-
         var legsTable = {
             id: "legs", alias: "Legs Data", columns: legs_cols
         };
 
         schemaCallback([resultsTable, teamsTable, divisionsTable, legsTable]);
-        // Once all our promises are resolved, we can call the schemaCallback to send this info to Tableau
-        // Promise.all([standardConnections, resultsTable, teamsTable, divisionsTable, legsTable]).then(function (data) {
-        //     schemaCallback(data[0], data[1], data[2], data[3], data[4]);
-        // });
-    };
-
-
-    // // Download the data
-    // myConnector.getData = function (table, doneCallback) {
-    //     // var yearsArray = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019];
-    //     var apiCall = "http://localhost:3333/output";
-
-    //     $.getJSON(apiCall, function (resp) {
-    //         var results = resp.results,
-    //             teams = resp.teams,
-    //             divisions = resp.divisions,
-    //             legs = resp.legs,
-    //             tableData = [];
-
-    //         if (table.tableInfo.id == "results") {
-    //             console.log(results);
-    //             var i, j
-    //             for (i in results) {
-    //                 for (j in results[i]) {
-    //                     tableData.push({
-    //                         "leg": i,
-    //                         "pa": results[i][j].pa,
-    //                         "dq": results[i][j].dq,
-    //                         "tn": results[i][j].tn,
-    //                         "dr": results[i][j].dr,
-    //                         "du": results[i][j].du,
-    //                         "rn": results[i][j].rn,
-    //                         "st": results[i][j].st,
-    //                         "ed": results[i][j].ed
-    //                     });
-    //                 }
-    //             }
-    //         }
-    //         if (table.tableInfo.id == "teams") {
-    //             console.log(teams);
-    //             var i, j;
-    //             for (i in teams) {
-    //                 for (j in teams[i].rs.ca) {
-    //                     for (j in teams[i].rs.rb) {
-    //                         for (j in teams[i].rs.ru) {
-    //                             for (j in teams[i].rs.xc) {
-    //                                 for (j in teams[i].rs.dh) {
-    //                                     for (j in teams[i].rs.ka) {
-    //                                         for (j in teams[i].rs.mb) {
-    //                                             tableData.push({
-    //                                                 "teamId": i,
-    //                                                 "tn": teams[i].tn,
-    //                                                 "dc": teams[i].dc,
-    //                                                 "ca1_n": teams[i].rs.ca[0].n,
-    //                                                 "ca1_g": teams[i].rs.ca[0].g,
-    //                                                 "ca2_n": teams[i].rs.ca[1].n,
-    //                                                 "ca2_g": teams[i].rs.ca[1].g,
-    //                                                 "rb_n": teams[i].rs.rb[j].n,
-    //                                                 "rb_g": teams[i].rs.rb[j].g,
-    //                                                 "ru_n": teams[i].rs.ru[j].n,
-    //                                                 "ru_g": teams[i].rs.ru[j].g,
-    //                                                 "xc_n": teams[i].rs.xc[j].n,
-    //                                                 "xc_g": teams[i].rs.xc[j].g,
-    //                                                 "dh_n": teams[i].rs.dh[j].n,
-    //                                                 "dh_g": teams[i].rs.dh[j].g,
-    //                                                 "ka_n": teams[i].rs.ka[j].n,
-    //                                                 "ka_g": teams[i].rs.ka[j].g,
-    //                                                 "mb_n": teams[i].rs.mb[j].n,
-    //                                                 "mb_g": teams[i].rs.mb[j].g
-    //                                             });
-    //                                         }
-    //                                     }
-    //                                 }
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         if (table.tableInfo.id == "divisions") {
-    //             console.log(divisions);
-    //             var i;
-    //             for (i in divisions) {
-    //                 tableData.push({
-    //                     "code": i,
-    //                     "long_name": divisions[i]
-    //                 });
-    //             }
-    //         }
-    //         if (table.tableInfo.id == "legs") {
-    //             console.log(legs);
-    //             for (var i = 0, len = legs.length; i < len; i++) {
-    //                 tableData.push({
-    //                     "code": legs[i].code,
-    //                     "short_name": legs[i].short_name,
-    //                     "long_name": legs[i].long_name
-    //                 });
-    //             }
-    //         }
-
-    //         table.appendRows(tableData);
-    //         console.log(tableData);
-
-    //         doneCallback();
-    //     });
-    // };
+    }
 
     myConnector.getData = function (table, doneCallback) {
         var url_beg = "http://localhost:3333/";
@@ -260,9 +139,9 @@
                     var i, j;
                     for (i in teams) {
                         for (j in teams[i].rs.ca) {
-                            if (!teams[i].rs.ca.hasOwnProperty(j)) {
-                                continue;
-                            }
+                            // if (teams[i].rs.ca[1] == "undefined") {
+                            //     continue;
+                            // }
                             for (j in teams[i].rs.rb) {
                                 for (j in teams[i].rs.ru) {
                                     for (j in teams[i].rs.xc) {
@@ -325,34 +204,9 @@
             });
         });
     };
-
     // Register the tableau connector, call this last
     tableau.registerConnector(myConnector);
 })();
-
-// // Helper function that loads a json and a callback to call once that file is loaded
-// function loadJSON(path, cb, isLocal) {
-//     var obj = new XMLHttpRequest();
-//     var url = "http://jsonplaceholder.typicode.com/" + path;
-//     obj.overrideMimeType("application/json");
-//     if (isLocal) {
-//         obj.open("GET", "../json/" + path + ".json", true);
-//     }
-//     else {
-//         obj.open("GET", url, true);
-//     }
-//     obj.onreadystatechange = function () {
-//         if (obj.readyState == 4 && obj.status == "200") {
-//             cb(obj.responseText);
-//         }
-//     }
-//     obj.send(null);
-// }
-
-// can probably remove since submit is contained in the click function
-// function send() {
-//     tableau.submit();
-// }
 
 $(document).ready(function () {
 

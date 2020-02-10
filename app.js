1b16b5f3e7be6761b4710de70f183c9398ae1444
SITRUK <kurtis.santillanes@gmail.com>
2020-02-10 10:55:12 -0800

    adding public folder to route change for heroku deployment

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + '/public'));
 // });
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.get('/2009', function (req, res) {

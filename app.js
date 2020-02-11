c6134bd5d15933bfd80df0268cd153023083f285
SITRUK <kurtis.santillanes@gmail.com>
2020-02-10 21:27:49 -0800

    trying localhost again ... yay2

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(__dirname + '/public'));
 //   res.redirect('/index.html');
 // });
 
-app.get('/*', function (req, res, next) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
